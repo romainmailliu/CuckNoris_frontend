1722d4c68d47f5943b35e83944ac9a17859b7c7c
Romain <romain.mailliu@gmail.com>
2026-02-12 15:57:36 +0100

    first commit

---
+++ components/Home.js
@@ -0,0 +1,7 @@
+import styles from "../styles/Home.module.css";
+
+function Home() {
+  return <div></div>;
+}
+
+export default Home;
---
+++ components/Login.js
@@ -0,0 +1,29 @@
+import styles from "../styles/Login.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEarlybirds } from "@fortawesome/free-solid-svg-icons";
+
+function Login() {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  return (
+    <div>
+      {/* Partie gauche */}
+      <div className={styles.left}>
+        <FontAwesomeIcon icon={faEarlybirds} />
+        <logo></logo>
+      </div>
+
+      {/* Partie droite */}
+      <div className={styles.right}>
+        <logo></logo>
+        <h1>See what's happening</h1>
+        <p>Join Hackatweet today</p>
+        <button>Sign up</button>
+        <p>Already have an account?</p>
+        <button>Sign in</button>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
